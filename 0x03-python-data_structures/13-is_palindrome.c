b36adeffd60faf391799cf700aba8b1847e458f3
R-oxy <aronmangati@gmail.com>
2023-06-12 02:33:53 -0700

    Adds a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome
@@ -11,33 +9,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL)
-        return (1);
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp;
 
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *temp;
-
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        temp = slow;
-        slow = slow->next;
-        temp->next = prev;
-        prev = temp;
-    }
-
-    if (fast != NULL)
-        slow = slow->next;
-
-    while (slow != NULL)
-    {
-        if (slow->n != prev->n)
-            return (0);
-        slow = slow->next;
-        prev = prev->next;
-    }
-
-    return (1);
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		temp = slow;
+		slow = slow->next;
+		temp->next = prev;
+		prev = temp;
+	}
+	if (fast != NULL)
+		slow = slow->next;
+	while (slow != NULL)
+	{
+		if (slow->n != prev->n)
+		{
+			return (0);
+		}
+		slow = slow->next;
+		prev = prev->next;
+	}
+	return (1);
 }
