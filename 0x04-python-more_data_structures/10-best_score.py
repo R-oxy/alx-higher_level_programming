c57a3255799c39451a87e88529bbff9429d0a5da
R-oxy <aronmangati@gmail.com>
2023-06-14 05:32:27 -0700

    Adds a function that returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    best_key = list(a_dictionary.keys())[0]
+    best_score = a_dictionary[best_key]
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best_key = key
+            best_score = value
+    return best_key
