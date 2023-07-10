583331e43fa7d7ca064b3cd7886e7ea13a04d662
R-oxy <aronmangati@gmail.com>
2023-07-10 03:47:42 -0700

    Adds a function that returns True if the object is exactly an instance of the specified class ; otherwise False

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list in sorted order (ascending)."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
