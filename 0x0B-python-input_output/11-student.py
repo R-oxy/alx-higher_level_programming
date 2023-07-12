2ae94fcdd35be4ac9c9dc098404fe798d3004bc0
R-oxy <aronmangati@gmail.com>
2023-07-12 02:09:47 -0700

    Implements Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""11-student.py"""
+
+
+class Student:
+    """class Student that defines a student by:
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    new_dict[item] = getattr(self, item)
+            return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
