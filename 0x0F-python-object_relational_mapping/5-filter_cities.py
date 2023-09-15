1f233d3d44653d389c36e1aa0fbbac4bbfd5ce5b
R-oxy <aronmangati@gmail.com>
2023-09-15 00:27:46 -0700

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+script that takes in the name of a state as an argument and lists all cities
+of that state, using the database `hbtn_0e_4_usa`.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name> "
+              "<state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database)
+        cursor = db.cursor()
+
+        query = (
+            "SELECT cities.name "
+            "FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id ASC"
+        )
+
+        cursor.execute(query, (state_name,))
+        cities = cursor.fetchall()
+
+        if cities:
+            city_names = ", ".join(city[0] for city in cities)
+            print(city_names)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
