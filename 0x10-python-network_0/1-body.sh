3d8ed7aaca0e8c3f4ca32b3c4e8bf3540b145c14
R-oxy <aronmangati@gmail.com>
2023-09-28 02:20:03 -0700

    new changes on cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET -L -I "$1" | awk 'NR == 1 { if ($2 == 200) print $0; else exit 1 } END { if (NR == 0) exit 1 }'
+curl -sL "$1" -X GET -D ./header -o ./output && { grep -q "200 OK" ./header && cat ./output; } || echo ""
