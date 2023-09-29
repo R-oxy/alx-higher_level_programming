c34744bf00532be46baf9205d548d24ecf48536e
R-oxy <aronmangati@gmail.com>
2023-09-29 06:12:29 -0700

    Implements Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Fetches and prints the 10 most recent commits
+from a GitHub repository.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./100-github_commits.py <repository_name> <owner_name>")
+        sys.exit(1)
+    else:
+        repo_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
+
+        try:
+            r = requests.get(url)
+            commits = r.json()
+
+            for commit in commits[:10]:
+                sha = commit.get("sha")
+                author = commit.get("commit").get("author").get("name")
+                print("{}: {}".format(sha, author))
+        except requests.exceptions.RequestException as e:
+            print("Error: {}".format(e))
