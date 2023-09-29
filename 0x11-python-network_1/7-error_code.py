509a29e574205f77cba376bed4398be3ced41185
R-oxy <aronmangati@gmail.com>
2023-09-29 04:32:19 -0700

    Implements Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a request to the given URL and displays the body of the response.
+If the HTTP status code is greater than or equal to 400,
+it prints an error message.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
