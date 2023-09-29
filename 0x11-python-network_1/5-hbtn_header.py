2773835383a1f83ca7ae8d2e33b08d78435c7c61
R-oxy <aronmangati@gmail.com>
2023-09-29 03:30:56 -0700

    Implements Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches the value of the X-Request-Id variable
+from the response header of a given URL.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    x_req_id = req.headers.get("X-Request-Id")
+    print(x_req_id)
