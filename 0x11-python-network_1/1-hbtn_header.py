dcb40f3dec32472ec0b8b9d47ada61cae4627215
R-oxy <aronmangati@gmail.com>
2023-10-01 23:22:37 -0700

    Fixes bugs on Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,11 +5,11 @@ from a given URL's HTTP response.
 """
 
 from urllib import request
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
     req = request.Request(url)
     with request.urlopen(req) as response:
         x_req_id = response.getheader('X-Request-Id')
