ed3b3571cb23ceec8ea7f8a3dfc6bf57ef537130
R-oxy <aronmangati@gmail.com>
2023-10-01 23:10:17 -0700

    Fixes Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Fetches the value of the X-Request-Id header
@@ -11,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url)
+    with request.urlopen(req) as response:
         x_req_id = response.getheader('X-Request-Id')
         print(x_req_id)
