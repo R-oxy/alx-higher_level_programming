a71255ba97f77f5ba2cf68393c3bdf115d765c2d
R-oxy <aronmangati@gmail.com>
2023-10-01 23:26:45 -0700

    Fixes bugs on Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,13 +4,13 @@ Fetches the value of the X-Request-Id header
 from a given URL's HTTP response.
 """
 
-from urllib import request
-from sys import argv
+import urllib.request
+import sys
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    req = request.Request(url)
-    with request.urlopen(req) as response:
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         x_req_id = response.getheader('X-Request-Id')
         print(x_req_id)
