3bc452a625bdeffb76ef9fa9ac69492e6c31e899
R-oxy <aronmangati@gmail.com>
2023-11-01 00:31:20 -0700

    Click and turn red

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* global $ */
+// Wait for the document to be ready
+$(document).ready(function () {
+  // Use the jQuery selector to select the <header> element
+  // Then use the `.css()` method to set the text color to red (#FF0000)
+  $('header').css('color', '#FF0000');
+});
