b3a41867c377babdb0ad8f507ed581d884320422
R-oxy <aronmangati@gmail.com>
2023-11-01 01:09:46 -0700

    Say Hello

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* global $ */
+
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* global $ */
+
+$(document).ready(function () {
+    $.get('https://hellosalut.stefanbohacek.dev/?lang=fr',
+	  function (data) {
+    $('#hello').text(data.hello);
+  });
+});
